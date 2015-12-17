47e1ec43ef982f9acf4bae90816c9d07b9970122
David McFarland <davidm@hb-studios.com>
2015-12-17 13:05:32 -0400

    add unity layer

--- layers/+frameworks/unity/config.el
+++ layers/+frameworks/unity/config.el
@@ -1,7 +1,5 @@
-(configuration-layer/declare-layers '(csharp cg yaml))
+(configuration-layer/declare-layers '(csharp cg))
 
 (add-to-list 'auto-mode-alist '("\\.compute\\'" . cg-mode))
 (add-to-list 'auto-mode-alist '("\\.cginc\\'" . cg-mode))
 (add-to-list 'auto-mode-alist '("\\.shader\\'" . cg-mode))
-(add-to-list 'auto-mode-alist '("\\.asset\\'" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.prefab\\'" . yaml-mode))
--- layers/+lang/cg/packages.el
+++ layers/+lang/cg/packages.el
@@ -1,12 +1,3 @@
-(setq cg-packages
-  '(
-	cg
-    ))
+(setq cg-packages '(cg))
 
-(defun cg/init-cg ()
-  (use-package cg
-    :defer t
-    :init
-	(add-to-list 'auto-mode-alist '("\\.compute\\'" . cg-mode))
-	(add-to-list 'auto-mode-alist '("\\.cginc\\'" . cg-mode))
-	(add-to-list 'auto-mode-alist '("\\.shader\\'" . cg-mode))))
+(defun cg/init-cg () (use-package cg :defer t))
