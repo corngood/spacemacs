41eb1f4a39349bf87924d5baef23a04dcf249089
David McFarland <corngood@gmail.com>
2016-03-26 00:22:35 -0300

    switch from cpputils-cmake to cmake-ide

--- layers/+tools/cmake/packages.el
+++ layers/+tools/cmake/packages.el
@@ -1,27 +1,33 @@
 (setq cmake-packages
       '(
         gdb-mi
-        cpputils-cmake
+        (cmake-ide :location (recipe :fetcher github :repo "corngood/cmake-ide"))
         ))
 
-(defun cmake/init-cpputils-cmake ()
+(defun cmake/init-cmake-ide ()
   (use-package cpputils-cmake
     :commands (cppcm-reload-all)
     :defer t
-    :init
-    (progn
-      (add-hook 'c-mode-hook 'cppcm-reload-all)
-      (add-hook 'c++-mode-hook 'cppcm-reload-all))
-    :config
-    (progn
-      (spacemacs/set-leader-keys "cR" 'cppcm-reload-all))))
+    :init (cmake-ide-setup)))
 
-(defun spacemacs/cmake-gdb ()
-  (interactive)
-  (let ((exe-path (cppcm-get-exe-path-current-buffer)))
-    (if exe-path
-        (gdb (concat "gdb -i=mi " exe-path))
-      (error "buffer has no executable path"))))
+;; (defun cmake/init-cpputils-cmake ()
+;;   (use-package cpputils-cmake
+;;     :commands (cppcm-reload-all)
+;;     :defer t
+;;     :init
+;;     (progn
+;;       (add-hook 'c-mode-hook 'cppcm-reload-all)
+;;       (add-hook 'c++-mode-hook 'cppcm-reload-all))
+;;     :config
+;;     (progn
+;;       (spacemacs/set-leader-keys "cR" 'cppcm-reload-all))))
 
-(defun cmake/post-init-gdb-mi ()
-  (spacemacs/set-leader-keys "d" 'spacemacs/cmake-gdb))
+;; (defun spacemacs/cmake-gdb ()
+;;   (interactive)
+;;   (let ((exe-path (cppcm-get-exe-path-current-buffer)))
+;;     (if exe-path
+;;         (gdb (concat "gdb -i=mi " exe-path))
+;;       (error "buffer has no executable path"))))
+
+;; (defun cmake/post-init-gdb-mi ()
+;;   (spacemacs/set-leader-keys "d" 'spacemacs/cmake-gdb))
