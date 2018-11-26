d47d510099cd0641ccfa15cbaf73a9decc4e7e24
bmag <bmagamb@gmail.com>
2018-11-26 08:50:51 +0200

    helm: disable ido-mode when helm is used
    Upstream helm forcibly disables ido-mode and throws an error when ido-mode and
    helm-mode are used together. We configure ido-mode but don't really use it by
    default, so best solution is to disable ido-mode.
    
    Note that ivy layer also disables ido-mode.

--- layers/+completion/helm/packages.el
+++ layers/+completion/helm/packages.el
@@ -44,6 +44,7 @@
                            :repo "emacsattic/helm-swoop"))
     (helm-spacemacs-help :location local)
     helm-xref
+    ido
     imenu
     persp-mode
     popwin
@@ -489,6 +490,13 @@
                                            xref-find-definitions-other-frame
                                            xref-find-references
                                            spacemacs/jump-to-definition))))
+
+(defun helm/post-init-ido ()
+  ;; helm is incompatible with ido, and spits an error when trying to enable
+  ;; both modes (see https://github.com/syl20bnr/spacemacs/issues/11640 and
+  ;; https://github.com/emacs-helm/helm/issues/2085)
+  (when (bound-and-true-p ido-mode)
+    (ido-mode -1)))
 
 (defun helm/post-init-imenu ()
   (spacemacs/set-leader-keys "ji" 'spacemacs/helm-jump-in-buffer))
