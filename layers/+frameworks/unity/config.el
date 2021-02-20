612377d063a7d77d87945ff5c124d3fc3158dc65
David McFarland <corngood@gmail.com>
2021-02-20 17:20:27 -0400

    add .hlsl to unity layer

--- layers/+frameworks/unity/config.el
+++ layers/+frameworks/unity/config.el
@@ -1,5 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.compute\\'" . cg-mode))
 (add-to-list 'auto-mode-alist '("\\.cginc\\'" . cg-mode))
 (add-to-list 'auto-mode-alist '("\\.shader\\'" . cg-mode))
+(add-to-list 'auto-mode-alist '("\\.hlsl\\'" . cg-mode))
 (add-to-list 'auto-mode-alist '("\\.asset\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.prefab\\'" . yaml-mode))
